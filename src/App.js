53c9ac535aee5d32ff9daa7ed5c5288305ae5a1c
iay <septianadip7998@gmail.com>
2024-05-08 00:14:57 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { MediaBox, Slider, Slide, Button } from "react-materialize";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tugas React JS Materialize</h1>
+      <Button>Hai</Button>
+    
     </div>
   );
 }
